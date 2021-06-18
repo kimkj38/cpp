4ab1e592a12ed71c2d5759ff39967490bd1f4e6d
kimkj38 <76815825+kimkj38@users.noreply.github.com>
2021-06-18 09:56:57 +0900

    Update 3_6.cpp

--- 3장/3_6.cpp
+++ 3장/3_6.cpp
@@ -42,7 +42,7 @@ int main(){
         cout << n << ' ';
     }
 
-    cout << endl << endl << "-- 2 에서 " << "4 까지의 랜덤 정수 10 개 --" << endl;
+    cout << endl << endl << "-- 2 에서 " << "10 까지의 랜덤 정수 10 개 --" << endl;
     for (int i = 0; i < 10; i++) {
         int n = r.nextInRange(2, 10);
         cout << n << ' ';
