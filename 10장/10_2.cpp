5480742fcb4646bde8e3486910d48973a56ebd3d
kimkj38 <76815825+kimkj38@users.noreply.github.com>
2021-12-10 12:56:14 +0900

    Create 10_2.cpp

---
+++ 10장/10_2.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+using namespace std;
+
+template <typename T>
+bool equalArrays(T* a, T* b, int size){
+    for(int i=0; i<size; i++){
+        if(a[i] != b[i])
+            return false;
+    }
+    return true;
+}
+
+int main(){
+    int x[] = {1, 10, 100, 5, 4};
+    int y[] = {1, 10, 100, 5, 4};
+    if(equalArrays(x,y,5)) cout << "같다";
+    else cout << "다르다";
+}
